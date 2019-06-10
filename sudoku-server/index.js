f0aadf579fd709e6ae897655d7c5be060b0d4712
康卜文 <kang3465@iCloud.com>
2019-06-10 19:37:26 +0800

    。

--- sudoku-server/index.js
+++ sudoku-server/index.js
@@ -7,4 +7,14 @@ app.use( async(ctx) => {
 })
 //监听3000接口
 app.listen(3000)
+io.on('connection', socket => {
+    console.log('初始化成功！下面可以用socket绑定事件和触发事件了');
+    socket.on('send', data => {
+        console.log('客户端发送的内容：', data);
+        socket.emit('getMsg', '我是返回的消息... ...');
+    })
+
+    setTimeout( () => {
+        socket.emit('getMsg', '我是初始化3s后的返回消息... ...')
+    }, 3001)
 console.log("demo in run")
