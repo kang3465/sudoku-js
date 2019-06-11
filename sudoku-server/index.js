5463a99ba53574610220ef0c5eb140040ea1d3ba
康卜文 <kang3465@iCloud.com>
2019-06-11 12:18:44 +0800

    1

--- sudoku-server/index.js
+++ sudoku-server/index.js
@@ -1,13 +1,21 @@
 const Koa = require('koa')
 const app = new Koa()
-const server = require('http').Server(app.callback());
+const Router = require('koa-router');
+const fs = require('fs');
+const server = require('http').createServer(app.callback());
 const io = require('socket.io')(server);
-app.use( async(ctx) => {
-    ctx.body = "hello world"
-})
+
+// 首页路由
+let router = new Router();
+router.get('/', ctx => {
+    ctx.response.type = 'html';
+    ctx.response.body = fs.createReadStream('./index.html');
+});
+app.use(router.routes());
+
 //监听3000接口
 app.listen(3000)
-io.on('connection', socket => {
+/*io.on('connection', socket => {
     console.log('初始化成功！下面可以用socket绑定事件和触发事件了');
     socket.on('send', data => {
         console.log('客户端发送的内容：', data);
@@ -17,5 +25,15 @@ io.on('connection', socket => {
     setTimeout( () => {
         socket.emit('getMsg', '我是初始化3s后的返回消息... ...')
     }, 3001)
-})
+})*/
+// socket连接
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: '+msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 console.log("demo in run")
