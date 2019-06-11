5eba7cda1301710defbd93bff735bb646ce8c58f
康卜文 <kang3465@iCloud.com>
2019-06-11 18:08:21 +0800

    添加socket.io

--- sudoku-server/index.js
+++ sudoku-server/index.js
@@ -1,5 +1,5 @@
-const Koa = require('koa')
-const app = new Koa()
+var Koa = require('koa');
+var app = new Koa();
 const Router = require('koa-router');
 const fs = require('fs');
 const server = require('http').createServer(app.callback());
@@ -13,19 +13,6 @@ router.get('/', ctx => {
 });
 app.use(router.routes());
 
-//监听3000接口
-app.listen(3000)
-/*io.on('connection', socket => {
-    console.log('初始化成功！下面可以用socket绑定事件和触发事件了');
-    socket.on('send', data => {
-        console.log('客户端发送的内容：', data);
-        socket.emit('getMsg', '我是返回的消息... ...');
-    })
-
-    setTimeout( () => {
-        socket.emit('getMsg', '我是初始化3s后的返回消息... ...')
-    }, 3001)
-})*/
 // socket连接
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
@@ -36,4 +23,8 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
     });
 });
-console.log("demo in run")
+
+// 监听端口
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
